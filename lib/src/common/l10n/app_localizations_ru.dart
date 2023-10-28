3da886448c1e2072ae36194c5d1298674079097c
abduraimovdev <kenzo778118@gmail.com>
2023-10-28 06:28:41 -0700

    Updated

---
+++ lib/src/common/l10n/app_localizations_ru.dart
@@ -0,0 +1,8 @@
+import 'app_localizations.dart';
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+
+}
---
+++ lib/src/common/l10n/app_localizations_uz.dart
@@ -0,0 +1,8 @@
+import 'app_localizations.dart';
+
+/// The translations for Uzbek (`uz`).
+class AppLocalizationsUz extends AppLocalizations {
+  AppLocalizationsUz([String locale = 'uz']) : super(locale);
+
+
+}
